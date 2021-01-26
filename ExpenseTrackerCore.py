de33260670ca38011b73ff29ba7530b553afdef8
Adi01 <adikakaam@gmail.com>
2021-01-26 22:56:36 +0530

    adding more flask changes

--- ExpenseTrackerCore.py
+++ ExpenseTrackerCore.py
@@ -1,9 +1,25 @@
 from flask import Flask
 app = Flask(__name__)
 
+#Home Page
 @app.route('/')
-def hello_world():
-   return 'Hello World’
+def homePage():
+    return 'Hello World1'
+
+#Username Page
+@app.route('/<username>')
+def userPage(username):
+    return 'Hi %s !' %username
+
+#Consolidated Page
+@app.route('/<username>/consolidated')
+def consolidatedPage(username):
+    return 'consolidated page for %s' %username
+
+#Report Page
+@app.route('/<username>/report')
+def reportPage(username):
+    return 'report page for %s' %username
 
 if __name__ == '__main__':
-   app.run()
+    app.run(debug = True)
